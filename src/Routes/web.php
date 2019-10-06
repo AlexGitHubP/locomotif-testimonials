0f004912966cf38d68ba2b260627fcdab5a891f9
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2019-10-06 17:50:57 +0300

    Update web.php
    Route change, to support middleware, more precisely "route model binding".

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,3 +1,12 @@
 <?php
-Route::resource('/testimonials', 'Locomotif\Testimonials\Controller\TestimonialsController');
+//Route::resource('/testimonials', 'Locomotif\Testimonials\Controller\TestimonialsController');
+Route::group(['middleware' => ['web']], function () {
+	Route::get('testimonials', 'Locomotif\Testimonials\Controller\TestimonialsController@index');
+	Route::post('testimonials', 'Locomotif\Testimonials\Controller\TestimonialsController@store');
+	Route::get('testimonials/create', 'Locomotif\Testimonials\Controller\TestimonialsController@create');
+	Route::get('testimonials/{testimonial}', 'Locomotif\Testimonials\Controller\TestimonialsController@show');
+	Route::get('testimonials/{testimonial}/edit', 'Locomotif\Testimonials\Controller\TestimonialsController@edit');
+	Route::put('testimonials/{testimonial}', 'Locomotif\Testimonials\Controller\TestimonialsController@update');
+	Route::delete('testimonials/{testimonial}', 'Locomotif\Testimonials\Controller\TestimonialsController@destroy');
+});
 ?>
