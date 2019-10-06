00d607276a34f824e43874611c3df0fcabcb1213
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2019-10-06 17:49:05 +0300

    Add files via upload
    Views for CRUD operations

---
+++ src/views/create.blade.php
@@ -0,0 +1,25 @@
+@extends('testimonials::header')
+
+@section('content')
+
+<a href="/testimonials"></a>
+
+<p>Create a testimonial</p>
+
+
+<form action="/testimonials" method="POST">
+    @csrf
+	<div class="form-group">
+		<label for="name">Name</label>
+		<input type="text" class="form-control" id="name"  name="name" value="">
+	</div>
+
+  	<div class="form-group">
+    	<label for="text">Description</label>
+    	<input type="text" class="form-control" id="text" name="text" value="">
+  	</div>
+  	<button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+@endsection
--- src/views/list.blade.php
+++ src/views/list.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
-<p>this is a blade template</p>
+<p>This is the listing page for Testimonials</p>
+
+<a href="/testimonials/create">Add a new testimonial</a>
+
+@foreach ($testimonials as $t)
+	<p>{{ $t->name }} <a href="/testimonials/{{ $t->id }}/edit">Edit</a> | <form action="/testimonials/{{ $t->id }}" method="POST">{{ method_field('DELETE') }} @csrf
+   	<input type="submit" class="btn btn-danger" value="Delete"/></form></p>
+@endforeach
 
 @endsection
