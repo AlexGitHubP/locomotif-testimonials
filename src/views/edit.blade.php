ff7762425c5f3c123e74959a6020dc9203e4db8c
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2019-10-20 14:54:54 +0300

    Add files via upload

--- src/views/edit.blade.php
+++ src/views/edit.blade.php
@@ -1,26 +1,25 @@
-@extends('admin::header')
-
+@extends('admin::inc/header')
+@section('title', 'Edit testimonial')
+@include('admin::inc/menu')
 @section('content')
+<div class="container">
+    <div class="cms-body">
+      <p>Update a testimonial</p>
+      <form action="/admin/testimonials/{{ $testimonial->id }}" method="POST">
+        {{ method_field('PUT') }}
+          @csrf
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" class="form-control" id="name"  name="name" value="{{ $testimonial->name }}">
+        </div>
 
-<a href="/admin/testimonials"></a>
-
-<p>Update a testimonial</p>
-
-
-<form action="/admin/testimonials/{{ $testimonial->id }}" method="POST">
-  {{ method_field('PUT') }}
-    @csrf
-  <div class="form-group">
-    <label for="name">Name</label>
-    <input type="text" class="form-control" id="name"  name="name" value="{{ $testimonial->name }}">
+          <div class="form-group">
+            <label for="text">Description</label>
+            <input type="text" class="form-control" id="text" name="text" value="{{ $testimonial->text }}">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
   </div>
-
-    <div class="form-group">
-      <label for="text">Description</label>
-      <input type="text" class="form-control" id="text" name="text" value="{{ $testimonial->text }}">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
+</div>
 
 @endsection
