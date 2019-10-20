e29d8d824a560c83ee88467af54f8aa0ecd98fe9
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2019-10-20 12:52:01 +0300

    Update web.php

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,12 +1,12 @@
 <?php
-//Route::resource('/testimonials', 'Locomotif\Testimonials\Controller\TestimonialsController');
+
 Route::group(['middleware' => ['web']], function () {
-	Route::get('testimonials', 'Locomotif\Testimonials\Controller\TestimonialsController@index');
-	Route::post('testimonials', 'Locomotif\Testimonials\Controller\TestimonialsController@store');
-	Route::get('testimonials/create', 'Locomotif\Testimonials\Controller\TestimonialsController@create');
-	Route::get('testimonials/{testimonial}', 'Locomotif\Testimonials\Controller\TestimonialsController@show');
-	Route::get('testimonials/{testimonial}/edit', 'Locomotif\Testimonials\Controller\TestimonialsController@edit');
-	Route::put('testimonials/{testimonial}', 'Locomotif\Testimonials\Controller\TestimonialsController@update');
-	Route::delete('testimonials/{testimonial}', 'Locomotif\Testimonials\Controller\TestimonialsController@destroy');
+	Route::get('admin/testimonials', 'Locomotif\Testimonials\Controller\TestimonialsController@index');
+	Route::post('admin/testimonials', 'Locomotif\Testimonials\Controller\TestimonialsController@store');
+	Route::get('admin/testimonials/create', 'Locomotif\Testimonials\Controller\TestimonialsController@create');
+	Route::get('admin/testimonials/{testimonial}', 'Locomotif\Testimonials\Controller\TestimonialsController@show');
+	Route::get('admin/testimonials/{testimonial}/edit', 'Locomotif\Testimonials\Controller\TestimonialsController@edit');
+	Route::put('admin/testimonials/{testimonial}', 'Locomotif\Testimonials\Controller\TestimonialsController@update');
+	Route::delete('admin/testimonials/{testimonial}', 'Locomotif\Testimonials\Controller\TestimonialsController@destroy');
 });
 ?>
