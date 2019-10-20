8bf844503e08e5cf429c2da7d79fc21aa43df139
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2019-10-20 12:52:31 +0300

    Add files via upload

--- src/views/create.blade.php
+++ src/views/create.blade.php
@@ -2,23 +2,23 @@
 
 @section('content')
 
-<a href="/testimonials"></a>
+<a href="/admin/testimonials"></a>
 
 <p>Create a testimonial</p>
 
 
-<form action="/testimonials" method="POST">
+<form action="/admin/testimonials" method="POST">
     @csrf
-	<div class="form-group">
-		<label for="name">Name</label>
-		<input type="text" class="form-control" id="name"  name="name" value="">
-	</div>
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" class="form-control" id="name"  name="name" value="">
+  </div>
 
-  	<div class="form-group">
-    	<label for="text">Description</label>
-    	<input type="text" class="form-control" id="text" name="text" value="">
-  	</div>
-  	<button type="submit" class="btn btn-primary">Submit</button>
+    <div class="form-group">
+      <label for="text">Description</label>
+      <input type="text" class="form-control" id="text" name="text" value="">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
 
--- src/views/edit.blade.php
+++ src/views/edit.blade.php
@@ -2,24 +2,24 @@
 
 @section('content')
 
-<a href="/testimonials"></a>
+<a href="/admin/testimonials"></a>
 
 <p>Update a testimonial</p>
 
 
-<form action="/testimonials/{{ $testimonial->id }}" method="POST">
+<form action="/admin/testimonials/{{ $testimonial->id }}" method="POST">
   {{ method_field('PUT') }}
     @csrf
-	<div class="form-group">
-		<label for="name">Name</label>
-		<input type="text" class="form-control" id="name"  name="name" value="{{ $testimonial->name }}">
-	</div>
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" class="form-control" id="name"  name="name" value="{{ $testimonial->name }}">
+  </div>
 
-  	<div class="form-group">
-    	<label for="text">Description</label>
-    	<input type="text" class="form-control" id="text" name="text" value="{{ $testimonial->text }}">
-  	</div>
-  	<button type="submit" class="btn btn-primary">Submit</button>
+    <div class="form-group">
+      <label for="text">Description</label>
+      <input type="text" class="form-control" id="text" name="text" value="{{ $testimonial->text }}">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
 
